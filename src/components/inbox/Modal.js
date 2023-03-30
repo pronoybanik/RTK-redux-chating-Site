c0b9597a9f6b3493c3844d4c4429f375e5c4b303
PRONOY <pronoybanik82@gmail.com>
2023-03-30 20:20:35 +0600

    email setup  verify

--- src/components/inbox/Modal.js
+++ src/components/inbox/Modal.js
@@ -1,4 +1,69 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { conversationsApi } from "../../features/conversations/conversationsApi";
+import { useGetUserQuery } from "../../features/user/userApi";
+import Error from "../ui/Error";
+import isValidEmail from "../ui/isValidEmail";
+
 export default function Modal({ open, control }) {
+
+    const [to, setTo] = useState("");
+    const [message, setMessage] = useState("");
+    const [userCheck, setUserCheck] = useState(false);
+    const { user: loggedInUser } = useSelector((state) => state.auth) || {};
+    const { email: myEmail } = loggedInUser || {};
+    const dispatch = useDispatch();
+    const [responseError, setResponseError] = useState("");
+    const [conversation, setConversation] = useState(undefined);
+
+    const { data: participant } = useGetUserQuery(to, {
+        skip: !userCheck,
+    });
+
+    useEffect(() => {
+        if (participant?.length > 0 && participant[0].email !== myEmail) {
+            // check conversation existance
+            dispatch(
+                conversationsApi.endpoints.getConversation.initiate({
+                    userEmail: myEmail,
+                    participantEmail: to,
+                })
+            )
+                .unwrap()
+                .then((data) => {
+                    setConversation(data);
+                })
+                .catch((err) => {
+                    setResponseError("There was a problem!");
+                });
+        }
+    }, [participant, dispatch, myEmail, to]);
+
+    const debounceHandler = (fn, delay) => {
+        let timeoutId;
+        return (...args) => {
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                fn(...args);
+            }, delay);
+        };
+    };
+
+    const doSearch = (value) => {
+        if (isValidEmail(value)) {
+            // check user API
+            setUserCheck(true);
+            setTo(value);
+        }
+    };
+
+    const handleSearch = debounceHandler(doSearch, 500);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form submitted");
+    };
+
     return (
         open && (
             <>
@@ -10,8 +75,7 @@ export default function Modal({ open, control }) {
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                         Send message
                     </h2>
-                    <form className="mt-8 space-y-6" action="#" method="POST">
-                        <input type="hidden" name="remember" value="true" />
+                    <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
                         <div className="rounded-md shadow-sm -space-y-px">
                             <div>
                                 <label htmlFor="to" className="sr-only">
@@ -20,10 +84,13 @@ export default function Modal({ open, control }) {
                                 <input
                                     id="to"
                                     name="to"
-                                    type="to"
+                                    type="email"
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                                     placeholder="Send to"
+                                    onChange={(e) =>
+                                        handleSearch(e.target.value)
+                                    }
                                 />
                             </div>
                             <div>
@@ -33,10 +100,12 @@ export default function Modal({ open, control }) {
                                 <textarea
                                     id="message"
                                     name="message"
-                                    type="message"
+                                    type="text"
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                                     placeholder="Message"
+                                    value={message}
+                                    onChange={(e) => setMessage(e.target.value)}
                                 />
                             </div>
                         </div>
@@ -45,15 +114,29 @@ export default function Modal({ open, control }) {
                             <button
                                 type="submit"
                                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
+                                disabled={
+                                    conversation === undefined ||
+                                    (participant?.length > 0 &&
+                                        participant[0].email === myEmail)
+                                }
                             >
                                 Send Message
                             </button>
                         </div>
 
-                        {/* <Error message="There was an error" /> */}
+                        {participant?.length === 0 && (
+                            <Error message="This user does not exist!" />
+                        )}
+                        {participant?.length > 0 &&
+                            participant[0].email === myEmail && (
+                                <Error message="You can not send message to yourself!" />
+                            )}
+
+                        {responseError && <Error message={responseError} />}
                     </form>
                 </div>
             </>
         )
     );
 }
+
